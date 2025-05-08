af49617c398f9e6d5baf28b013400d23c73a9c4b
anilbandari562 <anilbandari562@gmail.com>
2025-05-09 00:58:44 +0530

    my task-1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MyWebsiteComponent } from './my-website/my-website.component';
+import { HomeComponent } from './home/home.component';
+import { CalculatorComponent } from './calculator/calculator.component';
+import { RectangleComponent } from './rectangle/rectangle.component';
+import { CircleComponent } from './circle/circle.component';
+import { BMIComponent } from './bmi/bmi.component';
+import { DataBindingComponent } from './data-binding/data-binding.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {path:'My-Website',component:MyWebsiteComponent,children:[
+    {path:'Home',component:HomeComponent},
+    {path:'Welcome',component:WelcomeComponent},
+    {path:'Data-Binding',component:DataBindingComponent},
+    {path:'Calculator',component:CalculatorComponent}, 
+    {path:'Rectangle',component:RectangleComponent},
+    {path:'Circle',component:CircleComponent},
+    {path:'BMI',component:BMIComponent},
+  ]},
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { CalculatorComponent } from './calculator/calculator.component';
+import { RectangleComponent } from './rectangle/rectangle.component';
+import { CircleComponent } from './circle/circle.component';
+import { BMIComponent } from './bmi/bmi.component';
+import { LogoutComponent } from './logout/logout.component';
+import { MyWebsiteComponent } from './my-website/my-website.component';
+import { DataBindingComponent } from './data-binding/data-binding.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    WelcomeComponent,
+    CalculatorComponent,
+    RectangleComponent,
+    CircleComponent,
+    BMIComponent,
+    LogoutComponent,
+    MyWebsiteComponent,
+    DataBindingComponent
   ],
   imports: [
     BrowserModule,
